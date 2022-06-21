3d3de8b5585d7aca990019fa4eb210bf77deda01
Shallon Kobusinge <kobshallon041@gmail.com>
2022-06-21 21:15:06 +0200

    feat: starter

---
+++ src/main/java/com/data/emsjsp/EmsJspApplication.java
@@ -0,0 +1,13 @@
+package com.data.emsjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/data/emsjsp/EmsJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.emsjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
